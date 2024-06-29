ee9290cfabf2926327ec502a9d984a9e95fb8935
Djave17 <dsancheza@uamv.edu.ni>
2024-06-29 13:40:34 -0600

    Inicializacion

---
+++ Diplomado iOS/HolaMundo/HolaMundo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by David Sanchez on 29/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
